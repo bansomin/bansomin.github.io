d66ec11d27450fd4df793ccffa66939f0c048d83
bansomin <bansomin@users.noreply.github.com>
2016-12-03 17:03:14 +0800

    Update demo.js

--- MyGame/English/demo.js
+++ MyGame/English/demo.js
@@ -35,7 +35,7 @@
         dataType: "json",
         async: true,
         data: {
-            "userId":1233412,
+            "userId":345343,
             "userNick":nickName
         },
         type: "POST",
@@ -99,7 +99,7 @@ function showDiv(time){
         url: "http://211.157.179.218:8780/hxs_personaltutor_wechat/gameController/updateWGameList",
         dataType: "json",
         async: true,
-        data: { "userId":1233412,
+        data: { "userId":345343,
                 "gameTime":time,
         },
         type: "POST",
