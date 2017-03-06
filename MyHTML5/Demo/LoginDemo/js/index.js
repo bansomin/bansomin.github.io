809b504f66a855d7bb909df2d77b4f39c296266f
bansomin <good091231@126.com>
2017-03-06 11:56:22 +0800

    q
    q

---
+++ MyHTML5/Demo/LoginDemo/js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by HAO on 2017/2/28.
+ * Brief   :
+ * Version :
+ */
+
+$(function(){
+	$("button:eq(0)").mousedown(function(){
+		alert("Login");
+	})
+})
+
+$(function(){
+	$("button:eq(1)").mousedown(function(){
+		alert("SignUp");
+	})
+})
+
