a46ed2bf4c3cb125b3f18ea8f207711c7f5af4f0
haochang <bansomin@users.noreply.github.com>
2020-06-29 22:15:15 +0800

    分数动画 适配 数字

--- MyGame/MonsterMatch/main.9394d.js
+++ MyGame/MonsterMatch/main.9394d.js
@@ -151,7 +151,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.de926.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.d34a2.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -187,7 +187,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.76558.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.9394d.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -214,12 +214,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.44a7b.js');
+        require('src/settings.f764a.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.44a7b.js');
+        require('src/settings.f764a.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
