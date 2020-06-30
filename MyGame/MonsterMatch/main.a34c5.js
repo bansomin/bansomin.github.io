5c9a99607c20bd78f15d91957bdac360f2bd5b1b
haochang <bansomin@users.noreply.github.com>
2020-06-30 11:43:57 +0800

    update

--- MyGame/MonsterMatch/main.a34c5.js
+++ MyGame/MonsterMatch/main.a34c5.js
@@ -187,7 +187,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.737f8.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.a34c5.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -214,12 +214,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.cd54d.js');
+        require('src/settings.9b095.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.cd54d.js');
+        require('src/settings.9b095.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
