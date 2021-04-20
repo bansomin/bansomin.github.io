6c419c95a5122b1536211bf20a477c5035c451a0
HAO <good091231@126.com>
2021-04-20 11:07:53 +0800

    update

--- GCTurbo/chubby_dragon/src/settings.6e195.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/mainGame.fire",orientation:"",jsList:[],bundleVers:{internal:"f1413",resources:"58073",main:"ba547"}};
