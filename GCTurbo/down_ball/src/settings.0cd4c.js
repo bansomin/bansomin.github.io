21f02f865afd523d1a1146eee7378980d1cced4e
HAO <good091231@126.com>
2021-04-25 11:14:32 +0800

    sef

--- GCTurbo/down_ball/src/settings.0cd4c.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","UI"],collisionMatrix:[[true],[false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/landing.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"78230",internal:"fbb78",main:"815b4"}};
