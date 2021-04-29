3b00d9c9d18e20d718c8ce7fe6b7e1622c89befb
HAO <good091231@126.com>
2021-04-29 10:57:13 +0800

    as

---
+++ GCTurbo/beeconnect/src/settings.826bc.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/mainGame.fire",orientation:"",jsList:["assets/scripts/server/gta-amplitude.1e762.js"],bundleVers:{internal:"45e63",resources:"546f2",main:"bd491"}};
