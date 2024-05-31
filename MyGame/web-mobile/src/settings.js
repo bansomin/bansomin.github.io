b3f6e7723fc24d357e2768b1ce2891f9d1aaa51b
bansomin <good091231@126.com>
2024-05-31 15:08:49 +0800

    add web-mobile

---
+++ MyGame/web-mobile/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/demo01/BrightSaturaContrastUniform/BrightSaturaContrastUniform.fire",orientation:"landscape",jsList:["assets/libs/poly2tri.js"]};
