596dd347f9b4d680fa3e8275fb4c2d60a17f001a
bansomin <good091231@126.com>
2025-07-04 04:44:56 +0800

    更新

---
+++ MyGame/MarketMadness/web-mobile/src/settings.19bd6.js
@@ -0,0 +1,27 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/mainScene.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [
+        "assets/Script/utils/gameSpeed.44ea4.js",
+        "assets/Script/utils/runtime.36357.js"
+    ],
+    bundleVers: {
+        internal: "e71a3",
+        resources: "7dae9",
+        main: "73b35"
+    }
+};
